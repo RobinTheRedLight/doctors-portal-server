9a822b3e17a10eaf2bca22f88e6f74c13b71e966
RobinTheRedLight <robinmitra789@gmail.com>
2023-06-30 01:43:11 +0600

    JWT route added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
@@ -22,10 +23,28 @@ const client = new MongoClient(uri, {
     }
 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('Unauthorized Access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send('Unauthorized Access');
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 async function run() {
     try {
         const appointmentOptionCollection = client.db('DoctorsPortal').collection('appointmentOptions');
         const bookingsCollection = client.db('DoctorsPortal').collection('bookings');
+        const usersCollection = client.db('DoctorsPortal').collection('users');
         app.get('/appointmentOptions', async (req, res) => {
             const date = req.query.date;
             const query = {};
@@ -44,8 +63,13 @@ async function run() {
             res.send(options);
         });
 
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', verifyJWT, async (req, res) => {
             const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send('Unauthorized Access');
+            }
+            console.log('token', req.headers.authorization)
             const query = { email: email };
             const bookings = await bookingsCollection.find(query).toArray();
             res.send(bookings);
@@ -68,6 +92,23 @@ async function run() {
 
             const result = await bookingsCollection.insertOne(booking);
             res.json(result);
+        });
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email.toLowerCase();
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+                return res.send({ accessToken: token })
+            }
+            console.log(user);
+            res.status(403).send({ accessToken: '' });
+        })
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
         })
     } finally {
 
